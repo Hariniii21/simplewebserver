71986d86385e4bb932a161b439d162e77e5f1fa9
Hariniii21 <harinisubburam2006@gmail.com>
2024-03-15 09:26:49 +0530

    success

--- web.py
+++ web.py
@@ -1,64 +1,60 @@
 from http.server import HTTPServer,BaseHTTPRequestHandler
-
-content='''
-<!doctype html>
+content = """
+<!DOCTYPE html>
 <html>
-<head>
-<title> My Web Server</title>
-</head>
-<body>
-<h1>Top Five Revenue from Companies</h1>
-<table border=2>
-<tr>
-<th> Company Name</th>
-<th> Revenue</th>
-<th> Financial Year</th>
-</tr>
-
-<tr>
-<td> Micro Soft</td>
-<td> $86.6</td>
-<td> 2014</td>
-</tr>
-<tr>
-<td> Micro Soft</td>
-<td> $86.6</td>
-<td> 2014</td>
-</tr>
-<tr>
-<td> Micro Soft</td>
-<td> $86.6</td>
-<td> 2014</td>
-</tr>
-<tr>
-<td> Micro Soft</td>
-<td> $86.6</td>
-<td> 2014</td>
-</tr>
-<tr>
-<td> Micro Soft</td>
-<td> $86.6</td>
-<td> 2014</td>
-</tr>
-<tr>
-<td> Micro Soft</td>
-<td> $86.6</td>
-<td> 2014</td>
-</tr>
-</table>
-</body>
+     <title> Image Map </title>
+     <body>
+          <table border = "2" cellspacing = "10" cellpading = "6">
+               <caption> Top five revenue generating software companies</caption>
+               <tr>
+                    <th>Rank</th>
+                    <th>Company</th>
+                    <th>Revenue</th>
+                    <th>FY</th>
+               </tr>
+               <tr>
+                    <td>1</td>
+                    <td>Microsoft</td>
+                    <td>$86.8</td>
+                    <td>2014</td>
+               </tr>
+               <tr>
+                    <td>2</td>
+                    <td>Oracle</td>
+                    <td>$37.1</td>
+                    <td>2013</td>
+               </tr>
+               <tr>
+                    <td>3</td>
+                    <td>SAP</td>
+                    <td>$20.9</td>
+                    <td>2013</td>
+               </tr>
+               <tr>
+                    <td>4</td>
+                    <td>Symantec</td>
+                    <td>$6.8</td>
+                    <td>2013</td>
+               </tr>
+               <tr>
+                    <td>5</td>
+                    <td>VMware</td>
+                    <td>$5.2  </td>
+                    <td>2013</td>
+               </tr> 
+          </table>
+     </body>
 </html>
-'''
 
-class MyServer(BaseHTTPRequestHandler):
+"""
+class myhandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("Get request received...")
-        self.send_response(200) 
-        self.send_header("content-type", "text/html")       
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
         self.end_headers()
         self.wfile.write(content.encode())
-
-print("This is my webserver") 
-server_address =('',8000)
-httpd = HTTPServer(server_address,MyServer)
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
 httpd.serve_forever()
